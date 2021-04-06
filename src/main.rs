4b9f89e7af8a95942978172e33a3571a86359d03
Suvash Thapaliya <suvash@gmail.com>
2021-04-07 01:05:11 +0200

    alpa repl up and runnnig

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+struct Config<'a> {
+    name: &'a str,
+    version: &'a str,
+}
+
 fn main() {
-    println!("नमस्ते");
+    let config = Config {
+        name: env!("CARGO_PKG_NAME"),
+        version: env!("CARGO_PKG_VERSION"),
+    };
+    print_banner(config);
+
+    repl();
+}
+
+fn print_banner(config: Config) {
+    println!("{} version {}", config.name, config.version);
+    println!("Use Ctrl-C, or Ctrl-D to exit prompt");
+    println!();
+}
+
+fn repl() {
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
 }
